d747c2cf41f254cc0a2f2845688d31c068053b78
jekaplugarev <jeka_plugarev@mail.ru>
2021-07-05 20:21:33 +0800

    finish lesson 2

--- src/lessons/lesson2/lesson2.ts
+++ src/lessons/lesson2/lesson2.ts
@@ -20,9 +20,16 @@ console.log('lesson 2');
 // https://learn.javascript.ru/recursion
 // https://www.youtube.com/watch?v=Kuq6oIN3PH0
 
-
 // Task 01
 // Реализовать функцию sum которая суммирует 2 числа следующим образом sum(3)(6) === 9
+
+function sum(a: number) {
+    return function (b: number) {
+        return a + b
+    }
+}
+
+console.log(sum(3)(6))
 
 // Task 02
 // Реализовать функцию makeCounter которая работает следующим образом:
@@ -33,6 +40,21 @@ console.log('lesson 2');
 // counter2(); // 1
 // counter(); // 3
 
+function makeCounter() {
+    let count = 0
+    return function () {
+        return ++count
+    }
+}
+
+const counter = makeCounter();
+const counter2 = makeCounter();
+
+counter(); // 1
+counter(); // 2
+counter2(); // 1
+counter(); // 3
+
 // Task 03
 // Переписать функцию из Task 02 так, что бы она принимала число в качестве аргумента и это число было стартовым значением счетчика
 // и возвращала следующий объект методов:
@@ -40,6 +62,23 @@ console.log('lesson 2');
 // decrease: -1
 // reset: установить счетчик в 0;
 // set: установить счетчик в заданное значение;
+
+function counter3(n: number) {
+    let counterValue = n
+    return {
+        increase: () => counterValue += 1,
+        decrease: () => counterValue -= 1,
+        reset: () => {
+            counterValue = 0
+            return counterValue
+        },
+        set: (num: number) => {
+            counterValue = num
+            return counterValue
+        },
+        getCount: () => counterValue
+    }
+}
 
 // Task 04*
 // Реализовать функцию superSum которая принимает число в качестве аргумента, которое указывает на количество слагаемых
@@ -51,7 +90,37 @@ console.log('lesson 2');
 // 5) superSum(3)(2,5)(3) //10
 // 6) superSum(3)(2,5)(3,9) //10
 
-// P.S. типизируйте только аргументы, а при вызове функции используйте @ts-ignore
+// function superSum(n: number): number {
+//     if (n === 1) return n
+//     return n + superSum(n - 1)
+// }
+//
+// console.log(superSum(10))
+
+function superSum(n: number) {
+    if (n <= 0) return 0
+    if (n === 1) return (num: number) => num
+
+    let _arguments: number[] = []
+
+    function helper(...args: number[]) {
+        _arguments = [..._arguments, ...args]
+        if (_arguments.length >= n) {
+            _arguments.length = n
+            return _arguments.reduce((acc, item) => acc + item)
+        } else {
+            return helper
+        }
+    }
+    return helper
+}
+
+// @ts-ignore
+console.log(superSum(3)(2)(5)(3))
+// @ts-ignore
+console.log(superSum(3)(2,5,3))
+// @ts-ignore
+console.log(superSum(3)(2,5)(3,9))
 
 // Task 05
 // решить все задачи по рекурсии которые даны в конце статьи https://learn.javascript.ru/recursion
@@ -60,4 +129,5 @@ console.log('lesson 2');
 // написать функцию, которая повторяет функционал метода flat массива на всю глубину.
 
 // just a plug
-export default () => {};
+export default () => {
+};
